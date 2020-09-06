15b5ef96e4004fd06b17a3e4338a4382831e0f44
Maksym_Bielozor <booblikovich@gmail.com>
2020-09-06 09:59:47 +0300

    typo fix

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('dantist-crm app is running!');
+    expect(page.getTitleText()).toEqual('dentist-crm app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dantist-crm'`, () => {
+  it(`should have as title 'dentist-crm'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('dantist-crm');
+    expect(app.title).toEqual('dentist-crm');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('dantist-crm app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('dentist-crm app is running!');
   });
 });
